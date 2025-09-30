c98a7ff0db63779c2d56495b87cf4cedb2d9f37b
pumrum <pumrum@gmail.com>
2025-09-29 20:31:39 -0400

    fix indentation

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,9 @@
 def greet(name):
-       return f"Hello, {name}!"
+    return f"Hello, {name}!"
 
-   if __name__ == "__main__":
-       print(greet("World"))
-       print(greet("GitHub"))
+if __name__ == "__main__":
+    user_name = input("What's your name? ")
+    if user_name.strip():
+        print(greet(user_name))
+    else:
+        print("Hello, stranger!")
